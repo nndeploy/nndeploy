efa6eea63ffd25e7685022c93749d829d69235d6
Always <zhouhaoyitian@gmail.com>
2023-03-09 20:16:58 +0800

    chore: cmakelist and ignore

--- nntask/source/always/demo.cc
+++ nntask/source/always/demo.cc
@@ -1,6 +1,9 @@
 #include "nndeploy/include/base/include_c_cpp.h"
+#include "nndeploy/include/base/time_measurement.h"
 
 int main(int argc, char *argv[]) {
-    printf("hello world!\n");
-    return 0;
+  nndeploy::base::TimeMeasurement *tm = new nndeploy::base::TimeMeasurement();
+
+  printf("hello world!\n");
+  return 0;
 }
