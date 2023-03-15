b265f7c949a3dca27eb9f71455afed58255dcbbe
02200059Z <2018222066@email.szu.edu.cn>
2023-03-15 21:42:47 +0800

    <update>: time_measurement

--- nndeploy/source/base/time_measurement.cc
+++ nndeploy/source/base/time_measurement.cc
@@ -14,16 +14,28 @@ TimeMeasurement::TimeMeasurement() {}
 
 TimeMeasurement::start(const std::string& name) {
   clock_t start;
-  string* name_start = &name;
-}
 
-void end(const std::string& name) {
-  clock_t end;
-  string* name_end = name;
-  if (*name_start == *start_end) {
-    TimeMeasurement.duration
+  auto iter = time_record.find(name);
+  if (iter != time_record.end()) {
+    (iter->second)*.start = start;
+    record_check.push((iter->second)*);
+  } else {
+    time_record_seperate* name_now = new time_record_seperate(name, start);
+    time_record[name] = name_now;
+    record_check.push(name_now);
   }
 }
 
+void end(const std::string& name) {  //这里其实不需要输入参数也可以
+  clock_t end;
+  double duiration_now = end - (record_check.top()) *.start;
+  (record_check.top()) *.count_++;
+  (record_check.top())*.duiration_sum = (record_check.top())*.duiration_sum) +duiration_now;
+  record_check.pop();
+}
+
+void download(const std::string& path) {}
 }  // namespace base
+
+}  // namespace nndeploy
 }  // namespace nndeploy
