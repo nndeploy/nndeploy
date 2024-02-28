d8565fc4963468c9e983777e7c6cf37cd4961991
youxiudeshouyeren <1929724847@qq.com>
2024-02-28 11:02:34 +0800

    fix 建图bug

--- include/nndeploy/dag/util.h
+++ include/nndeploy/dag/util.h
@@ -17,6 +17,18 @@ namespace dag {
 bool checkEdge(const std::vector<Edge *> &src_edges,
                const std::vector<Edge *> &dst_edges);
 
+/**
+ * @brief 对vector插入不在vector中的元素，即类似集合的作用
+ * @tparam T 
+ * @param  vec              My Param doc
+ * @param  val              My Param doc
+ */
+template<typename T>
+void insertUnique(std::vector<T> &vec, const T &val) {
+  if (std::find(vec.begin(), vec.end(), val) == vec.end()) {
+    vec.emplace_back(val);
+  }
+}
 }  // namespace dag
 }  // namespace nndeploy
 
