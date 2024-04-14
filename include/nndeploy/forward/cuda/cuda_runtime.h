21f387a997c824762fe3dd700f51549f19d3cf8e
Always <zhouhaoyitian@gmail.com>
2024-04-14 22:25:40 +0800

    update: improve forward's runtime

---
+++ include/nndeploy/forward/cuda/cuda_runtime.h
@@ -0,0 +1,39 @@
+
+#ifndef _NNDEPLOY_FORWARD_CUDA_CUDA_RUNTIME_H_
+#define _NNDEPLOY_FORWARD_CUDA_CUDA_RUNTIME_H_
+
+#include "nndeploy/base/common.h"
+#include "nndeploy/base/glic_stl_include.h"
+#include "nndeploy/base/log.h"
+#include "nndeploy/base/macro.h"
+#include "nndeploy/base/object.h"
+#include "nndeploy/base/status.h"
+#include "nndeploy/base/string.h"
+#include "nndeploy/base/value.h"
+#include "nndeploy/forward/util.h"
+
+namespace nndeploy {
+namespace forward {
+
+class NNDEPLOY_CC_API CudaRuntime : public base::NonCopyable {
+ public:
+  CudaRuntime(){};
+  virtual ~CudaRuntime(){};
+
+  virtual base::Status init(
+      std::vector<TensorWrapper *> &tensor_repository,
+      std::vector<OpWrapper *> &op_repository,
+      std::map<std::string, device::Tensor *> weights) = 0;
+  virtual base::Status deinit() = 0;
+
+  virtual base::Status reshape(base::ShapeMap &shape_map) = 0;
+
+  virtual base::Status preRun() = 0;
+  virtual base::Status run() = 0;
+  virtual base::Status postRun() = 0;
+};
+
+}  // namespace forward
+}  // namespace nndeploy
+
+#endif
