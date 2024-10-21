42e3bcc8cad3751ecb8eb3ab49cfd58f79149558
youxiudeshouyeren <1929724847@qq.com>
2024-10-21 20:15:52 +0800

    测试图优化

---
+++ framework/include/nndeploy/op/op_relu.h
@@ -0,0 +1,26 @@
+
+#ifndef _NNDEPLOY_OP_OP_RELU_H_
+#define _NNDEPLOY_OP_OP_RELU_H_
+
+#include "nndeploy/ir/ir.h"
+#include "nndeploy/op/op.h"
+#include "nndeploy/op/op_unary.h"
+
+namespace nndeploy {
+namespace op {
+
+class OpRelu : public OpUnary {
+ public:
+  OpRelu() : OpUnary() { is_inplace_ = false; }
+  virtual ~OpRelu() {}
+
+  virtual base::Status run();
+};
+
+NNDEPLOY_CC_API base::Status relu(device::Tensor *input,
+                                     device::Tensor *output);
+
+}  // namespace op
+}  // namespace nndeploy
+
+#endif
