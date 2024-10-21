fe3a3d06732d0624a302e57de5000e823518982f
youxiudeshouyeren <1929724847@qq.com>
2024-10-21 20:15:52 +0800

    测试图优化

--- demo/inference/demo.cc
+++ demo/inference/demo.cc
@@ -1,13 +1,43 @@
-#include "flag.h"
-#include "nndeploy/base/glic_stl_include.h"
-#include "nndeploy/base/shape.h"
-#include "nndeploy/base/time_profiler.h"
+#include "nndeploy/ir/default_interpret.h"
+#include "nndeploy/ir/interpret.h"
+#include "nndeploy/ir/ir.h"
+#include "nndeploy/ir/op_param.h"
+#include "nndeploy/net/net.h"
+#include "nndeploy/op/expr.h"
+#include "nndeploy/op/op.h"
 
 using namespace nndeploy;
 
-void printHelloWorld() { std::cout << "hello world!" << std::endl; }
+class TestDesc : public ir::ModelDesc {
+ public:
+  void init() {
+    auto input =
+        op::makeInput(this, "input", base::dataTypeOf<float>(), {1, 1, 8, 8});
+    auto conv_param = std::make_shared<ir::ConvParam>();
+    conv_param->kernel_shape_ = {3, 3};
+    auto conv1 = op::makeConv(this, input, conv_param, "weight", "bias");
+    auto relu1 = op::makeRelu(this, conv1);
 
-int main(int argc, char const *argv[]) {
-  printHelloWorld();
-  return 0;
+    op::makeOutput(this, relu1);
+  }
+};
+
+int main() {
+  auto test_net = std::make_shared<net::Net>();
+  auto test_desc = new TestDesc();
+
+  test_desc->init();
+
+  test_desc->weights_["weight"] = new device::Tensor("weight");
+  test_desc->weights_["bias"] = new device::Tensor("bias");
+  for (auto x : test_desc->weights_) {
+    std::cout << x.first << std::endl;
+  }
+  test_desc->weights_["weight"]->reshape({32,1,3,3});
+  // test_desc->weights_["weight"]->setName("weight");
+  test_desc->weights_["bias"]->reshape({32});
+  // test_desc->weights_["bias"]->setName("bias");
+  test_net->setModelDesc(test_desc);
+  test_net->init();
+  test_net->dump(std::cout);
 }
