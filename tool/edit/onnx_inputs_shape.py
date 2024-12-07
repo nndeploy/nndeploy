6a9f15ae4377a68e1af2ccd94043d5da103c9825
Always <zhouhaoyitian@gmail.com>
2024-12-07 14:43:04 +0000

    提升onnx简单工具的易用性

--- tool/edit/onnx_inputs_shape.py
+++ tool/edit/onnx_inputs_shape.py
@@ -4,10 +4,10 @@ from onnx import shape_inference
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Modify input shapes of an ONNX model.')
-    parser.add_argument('--src_model_path', type=str, required=True, help='Path to the source ONNX model.')
-    parser.add_argument('--dst_model_path', type=str, required=True, help='Path to save the modified ONNX model.')
+    parser.add_argument('src_model_path', type=str, help='Path to the source ONNX model.')
+    parser.add_argument('dst_model_path', type=str, help='Path to save the modified ONNX model.')
     parser.add_argument('--input_shapes', type=str, required=True, help='Input shapes in the format "input1:1,3,224,224;input2:1,10".')
-    parser.add_argument('--is_infer_shape', action='store_true', help='Whether to infer shapes for the ONNX model.')
+    parser.add_argument('--infer_shape', action='store_true', help='Whether to infer shapes for the ONNX model.')
     return parser.parse_args()
 
 def parse_input_shapes(input_shapes_str):
@@ -18,7 +18,7 @@ def parse_input_shapes(input_shapes_str):
         input_shapes[name] = shape
     return input_shapes
 
-def write_onnx_input_shape(src_model_path, dst_model_path, input_shapes, is_infer_shape):
+def write_onnx_input_shape(src_model_path, dst_model_path, input_shapes, infer_shape):
     """
     修改 ONNX 模型的输入形状
 
@@ -26,7 +26,7 @@ def write_onnx_input_shape(src_model_path, dst_model_path, input_shapes, is_infe
     src_model_path (str): 源 ONNX 模型文件路径
     dst_model_path (str): 保存修改后 ONNX 模型的路径
     input_shapes (dict): 输入形状字典，键为输入名称，值为形状列表
-    is_infer_shape (bool): 是否推断 ONNX 模型的形状
+    infer_shape (bool): 是否推断 ONNX 模型的形状
 
     返回:
     None
@@ -37,12 +37,13 @@ def write_onnx_input_shape(src_model_path, dst_model_path, input_shapes, is_infe
     for input_tensor in graph.input:
         if input_tensor.name in input_shapes:
             shape = input_shapes[input_tensor.name]
-            input_tensor.type.tensor_type.shape.dim.clear()
+            while len(input_tensor.type.tensor_type.shape.dim) > 0:
+                input_tensor.type.tensor_type.shape.dim.pop()
             for dim in shape:
                 new_dim = input_tensor.type.tensor_type.shape.dim.add()
                 new_dim.dim_value = dim
 
-    if is_infer_shape:
+    if infer_shape:
         model = shape_inference.infer_shapes(model)
 
     onnx.checker.check_model(model)
@@ -53,9 +54,10 @@ if __name__ == '__main__':
     src_model_path = args.src_model_path
     dst_model_path = args.dst_model_path
     input_shapes = parse_input_shapes(args.input_shapes)
-    write_onnx_input_shape(src_model_path, dst_model_path, input_shapes, args.is_infer_shape)
+    write_onnx_input_shape(src_model_path, dst_model_path, input_shapes, args.infer_shape)
 
 # resnet
-## python3 onnx_inputs_shape.py --src_model_path /home/ascenduserdg01/github/nndeploy/build/resnet50-v1-7.onnx --dst_model_path /home/ascenduserdg01/github/nndeploy/build/resnet50-v1-7.staticshape.onnx --input_shapes data:1,3,224,224
-
+## python3 onnx_inputs_shape.py --src_model_path /home/ascenduserdg01/github/nndeploy/build/resnet50-v1-7.onnx /home/ascenduserdg01/github/nndeploy/build/resnet50-v1-7.staticshape.onnx --input_shapes data:1,3,224,224 --infer_shape
+# RMBGV1.4.onnx
+## python3 onnx_inputs_shape.py /home/ascenduserdg01/github/nndeploy/build/RMBGV1.4.onnx /home/ascenduserdg01/github/nndeploy/build/RMBGV1.4.staticshape.onnx --input_shapes input:1,3,1024,1024 --infer_shape
 
--- tool/edit/onnx_version.py
+++ tool/edit/onnx_version.py
@@ -13,9 +13,9 @@ from onnx import version_converter
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Convert ONNX model to a different version.')
-    parser.add_argument('--src_model_path', type=str, required=True, help='Path to the source ONNX model.')
-    parser.add_argument('--target_ir_version', type=int, required=True, help='Target ONNX version to convert to.')
+    parser.add_argument('src_model_path', type=str, help='Path to the source ONNX model.')
     parser.add_argument('--dst_model_path', type=str, default="", help='Path to save the converted ONNX model.')
+    parser.add_argument('--target_ir_version', type=int, required=True, help='Target ONNX version to convert to.')
     return parser.parse_args()
 
 def onnx_version_converter(src_model_path, target_ir_version, dst_model_path=""):
@@ -40,5 +40,5 @@ if __name__ == '__main__':
     onnx_version_converter(src_model_path, target_ir_version, dst_model_path)
 
 
-# python3 onnx_version.py --src_model_path /home/always/huggingface/nndeploy/model_zoo/detect/yolo/yolov8n.onnx --target_version 10
+# python3 onnx_version.py /home/always/huggingface/nndeploy/model_zoo/detect/yolo/yolov8n.onnx --target_version 10
     
