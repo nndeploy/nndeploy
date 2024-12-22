9c5a7e1589983cf4bd9742daf952664838ce9343
Always <zhouhaoyitian@gmail.com>
2024-12-22 05:04:17 +0000

    debug: fix test_mul error

--- python/nndeploy/test/op/test_mul.py
+++ python/nndeploy/test/op/test_mul.py
@@ -4,7 +4,7 @@ import torch
 import nndeploy
 from nndeploy.op import functional as F
 from nndeploy.base import device_name_to_code
-from nndeploy.test_utils import (
+from nndeploy.test.test_util import (
     createTensorFromNumpy,
     createNumpyFromTensor,
     device_name_to_code,
@@ -33,9 +33,9 @@ class TestMulOp(unittest.TestCase):
 
         nndeploy_result = ascend_result.to(device_name_to_code["cpu"])
 
-        ascend_input1_array = createNumpyFromTensor(ascend_input1)
-        diff_input = ascend_input1_array - np_input1
-        print("nndeploy和onnx输入的差异:", diff_input)
+        # ascend_input1_array = createNumpyFromTensor(ascend_input1)
+        # diff_input = ascend_input1_array - np_input1
+        # print("nndeploy和onnx输入的差异:", diff_input)
 
         self.assertTrue(
             np.allclose(
