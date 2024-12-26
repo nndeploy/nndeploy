f0b9665d1b671eb7e425f67746b86ecb4d730853
Arpan <azs0239@auburn.edu>
2024-12-25 22:46:45 -0600

    vector graph const. test

--- test/source/nndeploy/dag/graph_test.cc
+++ test/source/nndeploy/dag/graph_test.cc
@@ -31,17 +31,39 @@ TEST_F(GraphTest, GraphWithOneInputOutputEdge) {
     auto edge_in =  std::make_unique<Edge>("edge_in");
     auto edge_out = std::make_unique<Edge>("edge_out");
     auto graph = ConstructGraph("@@!!##$$", edge_in.get(), edge_out.get());
+    ASSERT_TRUE(graph->getConstructed());
     EXPECT_TRUE(graph->getName() == "@@!!##$$");
     EXPECT_TRUE(graph->getEdge("edge_in") == edge_in.get());
     EXPECT_TRUE(graph->getEdge("edge_out") == edge_out.get());
 }
 
+
 //This test fails, there may be chance of potential optimizations
 TEST_F(GraphTest, GraphWithDuplicateOutputEdge) {
     auto edge_in =  std::make_unique<Edge>("edge_in");
-    auto graph = ConstructGraph("@@!!##$$", edge_in.get(), edge_in.get());
-    EXPECT_FALSE(graph->getConstructed());
+    auto graph = ConstructGraph("3.141@@!!", edge_in.get(), edge_in.get());
+    EXPECT_FALSE(graph->getConstructed()); //is false
     EXPECT_FALSE(graph->getAllOutput()[0] == edge_in.get()); // this should false
-    EXPECT_TRUE(graph->getAllInput()[0] == edge_in.get()); //this should be fine 
+    EXPECT_TRUE(graph->getAllInput()[0] == edge_in.get()); //is true 
 }
 
+TEST_F(GraphTest, GraphWithVectorInputOutputEdge) {
+    auto inputs = std::vector<Edge *>();
+    auto outputs = std::vector<Edge *>();
+    inputs.emplace_back(new Edge("in_1"));
+    inputs.emplace_back(new Edge("in_2"));
+    inputs.emplace_back(new Edge("in_3"));
+    inputs.emplace_back(new Edge("in_4"));
+    outputs.emplace_back(new Edge("out_1"));
+    outputs.emplace_back(new Edge("out_2"));
+    outputs.emplace_back(new Edge("out_3"));
+    outputs.emplace_back(new Edge("out_4"));
+    auto graph = ConstructGraphWithVecArgs("@@!!##$$", inputs, outputs);
+    ASSERT_TRUE(graph->getConstructed());
+    ASSERT_EQ(graph->getAllInput().size(), 4);
+    ASSERT_EQ(graph->getAllOutput().size(), 4);
+    
+    //TODO: do this in ~GraphTest()
+    for(auto in_edge: inputs) {delete in_edge;}
+    for(auto out_edge: outputs) {delete out_edge;}
+}
