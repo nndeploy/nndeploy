c3dca1ca62af0c433143b9861f79728a87fe4f18
Arpan <azs0239@auburn.edu>
2024-12-25 19:20:27 -0600

    add fixture for GraphTest

--- test/source/nndeploy/dag/graph_test.cc
+++ test/source/nndeploy/dag/graph_test.cc
@@ -1,7 +1,28 @@
-// #include <gtest/gtest.h>
+#include <gtest/gtest.h>
+#include <memory>
+#include <vector>
 
-// #include "nndeploy/dag/graph.h"
-// using namespace nndeploy::dag;
-// TEST(graph_test, empty_graph) {
-//     Graph::Graph()
-// }
+#include "nndeploy/dag/edge.h"
+#include "nndeploy/dag/node.h"
+#include "nndeploy/dag/graph.h"
+using namespace nndeploy::dag;
+
+class GraphTest : public testing::Test {
+    protected:
+    std::unique_ptr<Graph> ConstructGraph(const std::string &name, 
+                                          Edge *input, Edge *output) {
+        return std::make_unique<Graph>(name, input, output);
+    }
+
+    std::unique_ptr<Graph> ConstructGraphWithVecArgs(const std::string &name, 
+                                                     std::vector<Edge *> inputs,
+                                                     std::vector<Edge *> outputs) {
+        return std::make_unique<Graph>(name, inputs, outputs);
+    }
+
+    std::unique_ptr<Graph> ConstructGraphWithInitLsArgs(const std::string &name, 
+                                                     std::initializer_list<Edge *> inputs,
+                                                     std::initializer_list<Edge *> outputs) {
+        return std::make_unique<Graph>(name, inputs, outputs);
+    }
+};
