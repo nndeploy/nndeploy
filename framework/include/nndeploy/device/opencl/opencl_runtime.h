51ff72bd121bf1e768183c6eff1567b161da40fd
Arpan <azs0239@auburn.edu>
2025-05-18 05:57:35 -0500

    ocl runtime init

---
+++ framework/include/nndeploy/device/opencl/opencl_runtime.h
@@ -0,0 +1,27 @@
+#ifndef _NNDEPLOY_DEVICE_OPENCL_OPENCL_RUNTIME_H_
+#define _NNDEPLOY_DEVICE_OPENCL_OPENCL_RUNTIME_H_
+
+#include "nndeploy/device/opencl/opencl_wrapper.h"
+
+namespace nndeploy
+{
+    namespace device
+    {
+        class OpenCLRuntime
+        {
+            private:
+            /* data */
+            public:
+                OpenCLRuntime(/* args */);
+                ~OpenCLRuntime();
+                base::Status init();
+
+                cl::Context* context();
+                uint8_t getPlatforms();
+        };
+
+}
+}
+
+#endif /* _NNDEPLOY_DEVICE_OPENCL_OPENCL_RUNTIME_H_ */
+
