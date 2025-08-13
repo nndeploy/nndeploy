eb54734cdda44bcf274e2892f3d064f30e7da52c
xnyh <976303224@qq.com>
2025-08-13 23:01:15 +0800

    [SAM] support sam dynamic graph

--- python/nndeploy/segment/__init__.py
+++ python/nndeploy/segment/__init__.py
@@ -2,5 +2,6 @@ try:
     from .result import ClassificationLableResult, ClassificationResult
     from .classification import ClassificationPostParam, ClassificationPostProcess, ClassificationGraph
     from .drawlabel import DrawLable
+    from .sam import SAMGraph, SAMPointNode, SAMPostProcess, SAMMaskNode, SelectPointNode
 except:
     pass
