22735036073536fa90fff24c8b94944f1cfd725a
csrdxka <lidongsheng101@gmail.com>
2025-09-10 13:42:17 +0800

    add loop demo

--- demo/loop/demo.cc
+++ demo/loop/demo.cc
@@ -15,11 +15,54 @@
 
 using namespace nndeploy;
 
+class ConstParam : public base::Param {
+ public:
+  int const_var = 1;
+};
+
+class ValParam : public base::Param {
+ public:
+  int val = 0;
+};
+
+class NNDEPLOY_CC_API ConstNode : public dag::Node {
+ public:
+  ConstNode(const std::string &name, std::vector<dag::Edge *> inputs,
+            std::vector<dag::Edge *> outputs)
+      : Node(name, inputs, outputs) {
+    key_ = "nndeploy::loop::ConstNode";
+    desc_ = "const variable";
+    this->setOutputTypeInfo<ValParam>();
+    node_type_ = dag::NodeType::kNodeTypeInput;
+  }
+  virtual ~ConstNode() {}
+
+  virtual base::Status run() {
+    setRunningFlag(true);
+    ValParam *val = new ValParam();
+    val->val = 10;
+    this->getOutput(0)->set(val, false);
+    this->getOutput(0)->notifyWritten(val);
+    setRunningFlag(false);
+    return base::kStatusCodeOk;
+  }
+};
+
 class NNDEPLOY_CC_API AddNode : public dag::Node {
  public:
   AddNode(const std::string &name, std::vector<dag::Edge *> inputs,
           std::vector<dag::Edge *> outputs)
-      : Node(name, inputs, outputs) {}
+      : Node(name, inputs, outputs) {
+    param_ = std::make_shared<ConstParam>();
+  }
+  virtual ~AddNode() {}
+
+  virtual base::Status run() {
+    setRunningFlag(true);
+    ConstParam *param = dynamic_cast<ConstParam *>(param_.get());
+    setRunningFlag(false);
+    return base::kStatusCodeOk;
+  }
 };
 
 int main(int argc, char **argv) {
