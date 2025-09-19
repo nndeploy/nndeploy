b4decc691f2aa07eb81355b98b850592fc08efb8
zhangzhaosen <zhangzhaosen@sina.com>
2025-09-19 16:37:26 +0800

    feat: markdown

--- app/workflow/src/pages/components/flow/nodeRegistry/CodeBlock/index.tsx
+++ app/workflow/src/pages/components/flow/nodeRegistry/CodeBlock/index.tsx
@@ -1,32 +1,44 @@
-
-//import ReactMarkdown from 'react-markdown';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-//import remarkGfm from 'remark-gfm';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import Markdown  from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import styles from './index.module.scss'
 
 interface CodeBlockProps {
   code: string;
-  language: string;
+  language?: string;
 }
 
 export default function CodeBlock({ code, language }: CodeBlockProps) {
   return (
-    <SyntaxHighlighter
-      language={language}
-      style={vscDarkPlus}
-      showLineNumbers
-      wrapLines
-      wrapLongLines
-      lineProps={{ style: { wordBreak: 'break-all', whiteSpace: 'pre-wrap' } }}
-      customStyle={{
-        margin: '0',
-        padding: '1em',
-        borderRadius: '4px',
-        backgroundColor: '#1e1e1e',
-        overflowX: 'hidden',
-      }}
-    >
-      {code}
-    </SyntaxHighlighter>
-  );
+    <div className={styles['markdown-body']}>
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({ node, 
+            //inline, 
+            className, children, ...props }) {
+            //const match = /language-(\w+)/.exec(className || '')
+            return  ( //  match && !inline 
+              <SyntaxHighlighter
+                style={vscDarkPlus}
+                language={language}
+                PreTag="div"
+                wrapLines={true}
+                customStyle={{
+                  background: 'transparent',
+                  margin: '12px 0',
+                  borderRadius: 8
+                }}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            )
+          }
+        }}
+      >
+        {code}
+      </Markdown>
+    </div>
+  )
 }
