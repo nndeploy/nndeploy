6724a2e09ce1b67c50390d8bb284b1adb6ba624f
zhangzhaosen <zhangzhaosen@sina.com>
2025-11-06 11:38:31 +0800

    fix: markdonw bash language style

--- app/workflow/src/pages/components/CodeBlock/index.tsx
+++ app/workflow/src/pages/components/CodeBlock/index.tsx
@@ -9,9 +9,12 @@ import 'github-markdown-css';
 import { ReactNode } from 'react'
 
 interface CodeBlockProps {
+  inline?: boolean;
   code: ReactNode;
   language?: string;
 }
+
+
 
 export default function CodeBlock({ code, language }: CodeBlockProps) {
   return (
@@ -23,14 +26,34 @@ export default function CodeBlock({ code, language }: CodeBlockProps) {
         components={{
           code({ node,
             //inline, 
-            className, children, ...props }) {
+            className,  children, ...props }) {
+
+              // 通过检查className来判断是否是行内代码
+            const isInline = !className?.includes('language-');
+
+            if (isInline) {
+              return (
+                <code
+                  className={className}
+                  style={{
+                    background: '#f6f8fa',
+                    padding: '0.2em 0.4em',
+                    borderRadius: 3,
+                    color: '#24292e'
+                  }}
+                  {...props}
+                >
+                  {children}
+                </code>
+              );
+            }
             //const match = /language-(\w+)/.exec(className || '')
             return ( //  match && !inline 
               <SyntaxHighlighter
                 style={vscDarkPlus}
                 language={language}
 
-                PreTag="code"
+                PreTag="pre"
                 wrapLines={true}
                 customStyle={{
                   // background: 'transparent',
@@ -39,17 +62,16 @@ export default function CodeBlock({ code, language }: CodeBlockProps) {
                   background: '#f6f8fa',
                   color: '#24292e',
                   borderRadius: '6px',
-                  padding: '.2em .4em',
-                 // padding: '16px',
+                  padding: '16px',
                   fontSize: '85%',
-                  //lineHeight: 1.5
+                  lineHeight: 1.5
                 }}
                 codeTagProps={{
                   style: {
                     fontFamily: 'SFMono-Regular, Consolas, Liberation Mono, Menlo, monospace',
                     // 注释颜色调整
                     //color: '#6a737d!importent',
-                    
+
                   }
                 }}
               >
